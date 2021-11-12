de533f243b53651045534be0ca231e684e22da1d
Shaffaaf Ahmed <shaffaaf.ahmed@gmail.com>
2021-11-12 16:39:31 +0500

    addedadde

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,12 +1,8 @@
-tag_name) @tag
+(tag_name) @tag
+(id) @id
+(class) @class
 (erroneous_end_tag_name) @tag.error
 (doctype) @constant
 (attribute_name) @attribute
 (attribute_value) @string
 (comment) @comment
-
-[
-  "<"
-  ">"
-  "</"
-] @punctuation.bracket
